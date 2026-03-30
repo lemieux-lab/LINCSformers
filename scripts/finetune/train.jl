4465de1495d88a45658329b9158d5413adb35023
Serena Chan <serenaktc@gmail.com>
2026-03-30 16:33:49 -0400

    added oversampling, pca stuff

--- scripts/finetune/train.jl
+++ scripts/finetune/train.jl
@@ -1,28 +1,42 @@
 using Flux, ProgressBars, MultivariateStats, Statistics
 
-function ce_loss(model, x, x_pca, y, modeltype)
-    if modeltype == "rtf"
-        logits = model(x)
-    else
+function ce_loss(model, x, x_pca, y, use_pca)
+    if use_pca
         logits = model(x, x_pca)
+    else
+        logits = model(x)
     end
     return Flux.logitcrossentropy(logits, y)
 end
 
-function train(epochs, train_losses, test_losses, preds, trues, loss_fn)
+function train(epochs, train_losses, test_losses, preds, trues, loss_fn, use_oversampling, class_to_idx, avail_classes, use_pca, batch_size)
     for epoch in ProgressBar(1:epochs)
+
+        iter = div(size(X_train, 2), batch_size)
         
         Flux.trainmode!(ft_model)
         train_epoch_losses = Float32[]
         
-        for start_idx in 1:batch_size:size(X_train, 2)
-            end_idx = min(start_idx + batch_size - 1, size(X_train, 2))
+        # for start_idx in 1:batch_size:size(X_train, 2)
+            # end_idx = min(start_idx + batch_size - 1, size(X_train, 2))
+        for i in 1:iter
 
-            x_gpu = gpu(Int32.(X_train[:, start_idx:end_idx]))
-            y_gpu = gpu(Float32.(y_train[:, start_idx:end_idx]))
+            # x_gpu = gpu(Int32.(X_train[:, start_idx:end_idx]))
+            # y_gpu = gpu(Float32.(y_train[:, start_idx:end_idx]))
 
-            if modeltype == "v1" || modeltype == "v2"
-                raw_batch_cpu = raw_train_norm[:, start_idx:end_idx]
+            if use_oversampling
+                batch_idx = oversample_batch(class_to_idx, avail_classes, batch_size)
+            else
+                start_idx = (i - 1) * batch_size + 1
+                end_idx = min(start_idx + batch_size - 1, size(X_train, 2))
+                batch_idx = start_idx:end_idx
+            end
+
+            x_gpu = gpu(Int32.(X_train[:, batch_idx]))
+            y_gpu = gpu(Float32.(y_train[:, batch_idx]))
+
+            if use_pca
+                raw_batch_cpu = raw_train_norm[:, batch_idx]
                 x_pca_cpu = MultivariateStats.predict(pca_train_norm, raw_batch_cpu)
                 x_pca = gpu(Float32.(x_pca_cpu))
             else
@@ -30,10 +44,12 @@ function train(epochs, train_losses, test_losses, preds, trues, loss_fn)
             end
 
             lv, grads = Flux.withgradient(ft_model) do m
-                loss_fn(m, x_gpu, x_pca, y_gpu, modeltype)
+                loss_fn(m, x_gpu, x_pca, y_gpu, use_pca)
             end
             Flux.update!(opt, ft_model, grads[1])
-            lv = loss_fn(model, x_gpu, x_pca, y_gpu, modeltype)
+
+            lv = loss_fn(ft_model, x_gpu, x_pca, y_gpu, use_pca)
+
             push!(train_epoch_losses, lv) 
         end
         push!(train_losses, mean(train_epoch_losses))
@@ -47,7 +63,7 @@ function train(epochs, train_losses, test_losses, preds, trues, loss_fn)
             x_gpu = gpu(Int32.(X_test[:, start_idx:end_idx]))
             y_gpu = gpu(Float32.(y_test[:, start_idx:end_idx]))
 
-            if modeltype == "v1" || modeltype == "v2"
+            if use_pca
                 raw_batch_cpu = raw_test_norm[:, start_idx:end_idx]
                 x_pca_cpu = MultivariateStats.predict(pca_train_norm, raw_batch_cpu)
                 x_pca = gpu(Float32.(x_pca_cpu))
